789c1e430ae1c93aa54fbac9968f6451d5fe302b
khmhmm <khmhmm@ya.ru>
2021-09-13 15:31:10 +0400

    Added macro

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+/// Allows you create a cycle with mutable conditions.
+/// # Arguments list:
+/// - var: step-variable
+/// - cond: mutable condition (for example, array length)
+/// - exp: expression breaking the cycle (for example, closure: |a,b| a==b or std::cmp::PartialEq::eq)
+/// - code: following code
+/// # Examples
+/// ```
+///  fn main() {
+///    let mut v = vec![1i32,2,3,4,5];
+///    
+///    let mut i = 0;
+///    while_mut!{
+///        i, v.len(), |a:&usize,b:&usize| a==b,
+///        {
+///            v.remove(0);
+///            println!("{} {} {}", v[0], v.len(), i);
+///            i += 1;
+///        }
+///    }
+/// }
+/// ```
+#[macro_export]
+macro_rules! while_mut{
+    {$var: ident, $cond: expr, $exp: expr, $code: block} => {
+        let mut c = $cond;
+        loop {
+            if $exp(&$var,&c) { break; }
+            c = $cond;
+            $code
+        }
+    }
+}
+
+fn main() {}
